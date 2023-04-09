f9ab8579456d299e670c641527045dedb0ef0068
pulzed <danijel.durakovic@gmail.com>
2023-04-09 11:54:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+![YABS.js](/logo.png?raw=true)
+
+YABS.js is a lightweight JavaScript build system.
+
+( **Currently in development** -- NOT production ready )
+
+## Prerequisites
+
+- [uglify-js](https://www.npmjs.com/package/uglify-js) ( install with "npm -g install uglify-js" )
+- [preprocessor.js](https://www.npmjs.com/package/preprocessor) ( install with "npm -g install preprocessor" ) - only needed if the build leverages the preprocessor
+
+
+## How it works
+
+YABS.js takes a single JSON file containing the build instructions as an input. It then verifies, prepares and invokes the build process.
+
+## Minimal example
+
+todo
+
+## License
+
+Copyright (c) 2023 Danijel Durakovic
+
+MIT License
---
+++ yabs.js
@@ -0,0 +1,79 @@
+/*
+ *   __ __ _____ _____ _____     _
+ *  |  |  |  _  |  _  |   __|   |_|___
+ *   \_   |     |  _ -|__   |_  | |_ -|
+ *    /__/|__|__|_____|_____|_|_| |___|
+ *                            |___|
+ *  Yet-Another-Build-System.js
+ *  (c) 2023 Danijel Durakovic
+ *
+ */
+
+/*jshint esversion:6*/
+
+const path = require('path');
+const fs = require('fs');
+const { exec } = require('child_process');
+const { EOL } = require('os');
+
+const BUILDSYS_GLOBALS = {
+	// build system version
+	VERSION: '0.0.1',
+	// build output directory
+	OUTPUT: 'build'
+};
+
+const YABS = (function() {
+
+	// detect whether output is a terminal or not
+	// (so color codes can be stripped for redirected output)
+	const IS_TTY = process.stdout.isTTY;
+
+	// logger output constants
+	const OUTPUT_RESET    = (IS_TTY) ? '\x1b[0m'  : '';
+	const OUTPUT_BRIGHT   = (IS_TTY) ? '\x1b[1m'  : '';
+	const OUTPUT_FG_BLACK = (IS_TTY) ? '\x1b[30m' : '';
+	const OUTPUT_FG_GREEN = (IS_TTY) ? '\x1b[32m' : '';
+	const OUTPUT_FG_RED   = (IS_TTY) ? '\x1b[31m' : '';
+
+	const log = {
+		out: function(msg) {
+			process.stdout.write(msg + '\n');
+		},
+		endl: function() {
+			process.stdout.write('\n');
+		},
+		error: function(message) {
+			process.stdout.write(OUTPUT_FG_RED + '\\!/ ' + OUTPUT_RESET + message + '\n');
+		},
+		header: function() {
+			log.out('  __ __ _____ _____ _____     _');
+			log.out(' |  |  |  _  |  _  |   __|   |_|___');
+			log.out('  \\_   |     |  _ -|__   |_  | |_ -|');
+			log.out('   /__/|__|__|_____|_____|_|_| |___|');
+			log.out('                           |___|');
+			//log.endl();
+			//log.out(' '.repeat(31 - BUILDSYS_GLOBALS.VERSION.length) + '[ v' + BUILDSYS_GLOBALS.VERSION + ' ]');
+			log.out(' ( Yet');
+			log.out('   Another' + ' '.repeat(28 - BUILDSYS_GLOBALS.VERSION.length) + '[ v' + BUILDSYS_GLOBALS.VERSION + ' ]');
+			log.out('   Build');
+			log.out('   System ).js   (c) 2023 Danijel Durakovic');
+			log.endl();
+			log.out('  - - - - - - - - - - - - - - - - - - - - -');
+			log.endl();
+		},
+		help: function() {
+		}
+	};
+
+	return {
+		run: function(argv) {
+			log.header();
+			log.error('Missing input file!');
+			log.endl();
+			log.out('Use --help to display help.');
+		}
+	};
+}());
+
+YABS.run(process.argv);
