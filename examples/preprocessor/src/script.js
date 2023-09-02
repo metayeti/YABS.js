9e97ed9b65803f66de8c511771e738e72a739fa8
pulzed <danijel.durakovic@gmail.com>
2023-09-02 17:13:33 +0200

    Add explanation to comment

--- examples/preprocessor/src/script.js
+++ examples/preprocessor/src/script.js
@@ -1,3 +1,7 @@
+/* To build this example in debug "mode" (simulated), use: node yabs.js examples/preprocessor -debug */
+/* You will note that with the code below, the local development code will behave as if it's running in debug,
+ * meaning it will skip over all the comments. In release "mode", the lines after ?= will be outputted to script. */
+
 /* First let's check the type of build and configure for release. */
 
 //? if (typeof DEBUG === 'undefined') DEBUG = false;
@@ -6,6 +10,6 @@
 /* If this is the release version, change some text in the document. */
 
 //? if (RELEASE) {
-//? write("const $firstp = document.querySelector('p');");
-//? write("$firstp.innerHTML = 'This example demonstrates preprocessor features. This example is now running in &rsquo;RELEASE&rsquo; mode.';");
+//?= "const $firstp = document.querySelector('p');"
+//?= "$firstp.innerHTML = 'This example demonstrates preprocessor features. This example is now running in &rsquo;RELEASE&rsquo; mode.';"
 //? }
