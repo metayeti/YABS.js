6b3bc12df90adaf15de3c56452afd886bd546d55
pulzed <danijel.durakovic@gmail.com>
2023-09-02 22:24:39 +0200

    Add example game music

--- examples/game/src/game.js
+++ examples/game/src/game.js
@@ -10,4 +10,25 @@
  * @version 1.0.0
  */
 
+const mainState  = new myst.State();
 
+mainState.draw = function() {
+	this.surface.clear();
+	this.paint.rectFill(10, 10, 20, 20, 'red');
+};
+
+mainState.update = function() {
+};
+
+//
+// game object
+//
+const game = new myst.Game({
+	canvasId: 'game',
+	state: mainState,
+	viewMode: 'center',
+	simpleLoop: true
+});
+
+// run game on window load
+window.addEventListener('load', game.run);
