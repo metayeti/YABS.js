f942602f329c5792132e6db4ebce035f82d2206c
pulzed <danijel.durakovic@gmail.com>
2023-09-03 12:23:27 +0200

    Change comments in preprocessor example source

--- examples/preprocessor/src/script.js
+++ examples/preprocessor/src/script.js
@@ -1,14 +1,11 @@
 /* To build this example in debug "mode" (simulated), use: node yabs.js examples/preprocessor -debug */
 
-/* You will note that with the code below, the local development code will behave as if it's running in debug,
- * meaning it will skip over all the comments. In release "mode", the lines after ?= will be outputted to script. */
-
 // Note: normally, the preprocessor only runs when invoking a build with a -variable
 // parameter. Since we want this file to be run through the preprocessor when no
 // parameters are invoked, we need to "force" the use of the preprocessor by setting
 // the "preprocess" option to true in build.json for this specific script.
 
-/* First let's check the type of build and configure for release. */
+/* First let's check the type of build and configure variables. */
 
 //? if (typeof DEBUG === 'undefined') DEBUG = false;
 //? const RELEASE = !DEBUG;
@@ -22,8 +19,9 @@
 
 /* If this is the debug version, we can add features as plain code.
  * This will make it so that when running locally, the code will behave
- * as if the meta comments are just normal comments. When building, the preprocessor will kick in
- * and omit the code below, unless compiled with -debug. */
+ * as if the meta comments are just normal comments. When building, the
+ * preprocessor will kick in and omit the code below, unless compiled
+ * with -debug. */
 
 //? if (DEBUG) {
 	console.log('This message will only show up in debug mode!');
