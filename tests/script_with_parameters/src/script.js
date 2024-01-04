be23788ce345c60598ff2ce6dc05d100e25bbf2a
pulzed <danijel.durakovic@gmail.com>
2024-01-04 20:49:35 +0100

    Add test script_with_parameters

---
+++ tests/script_with_parameters/src/script.js
@@ -0,0 +1,10 @@
+{
+	const scripts = document.getElementsByTagName('script');
+	const script = scripts[scripts.length - 1];
+	const src_split = script.src.split('/');
+	const parsed_src = src_split.at(-1);
+	const $p = document.createElement('p');
+	$p.innerHTML = `If this text shows, the script <span class="highlight">${parsed_src}</span> has loaded successfully.`;
+	document.getElementById('content').appendChild($p);
+}
+
