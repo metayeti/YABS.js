96a57bb1b8ed872fe8afd10048997ae4b568246e
pulzed <danijel.durakovic@gmail.com>
2024-01-08 21:32:39 +0100

    Add preprocessor test

---
+++ tests/10_preproc_default/src/script.js
@@ -0,0 +1,29 @@
+console.log(
+	'Is debug set?',
+	//?= DEBUG
+);
+
+//? if (DEBUG) {
+//
+	console.log('This test is running in debug mode.');
+//? } else {
+	console.log('This test is running in release mode.');
+//? }
+
+
+//? if (0) {
+
+//? if (FLAGA) {
+console.log('Flag A is set');
+//? } else {
+console.log('Flag A is not set');
+//? }
+
+
+//? if (FLAGB) {
+console.log('Flag B is set');
+//? } else {
+console.log('Flag B is not set');
+//? }
+
+//? }
